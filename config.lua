f39543673682f587e3b4fe9fab9f7cf0bf62faca
WaterGod1723 <87053221+WaterGod1723@users.noreply.github.com>
2025-01-09 17:09:19 +0800

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,9 @@
+-- 协议、主机、地址重写, 该函数会被代理程序调用，用于protocol, host, path这三个值的重写
+function GoRequest(protocol, host, path)
+  return protocol, host, path
+end
+
+-- 代理服务器配置，会被代理程序调用，根据域名来返回代理配置，为""时表示直连
+function GoProxy(host)
+  return "http://usr:pwd@proxy.com:8080"
+end
