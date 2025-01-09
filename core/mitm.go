473c6fe24f05c5ae280b6a1799e47ce124cb0f22
WaterGod1723 <87053221+WaterGod1723@users.noreply.github.com>
2025-01-09 17:03:51 +0800

    Update mitm.go

--- core/mitm.go
+++ core/mitm.go
@@ -47,3 +47,56 @@ func (client *Client) Read(b []byte) (n int, err error) {
 		return (*client.conn).Read(b)
 	}
 }
+
+type Server struct {
+	isTls   bool
+	conn    *net.Conn
+	tlsConn *tls.Conn
+	reader  *bufio.Reader
+}
+
+func (server *Server) ReadResponse(req *http.Request) (*http.Response, error) {
+	return http.ReadResponse(server.reader, req)
+}
+
+func (server *Server) Write(b []byte) (n int, err error) {
+	if server.isTls {
+		return server.tlsConn.Write(b)
+	} else {
+		return (*server.conn).Write(b)
+	}
+}
+
+func (server *Server) Read(b []byte) (n int, err error) {
+	if server.isTls {
+		return server.tlsConn.Read(b)
+	} else {
+		return (*server.conn).Read(b)
+	}
+}
+
+type Intermediary struct {
+	client Client
+	server map[string]*Server
+}
+
+// [协议，代理地址，账号，密码]
+type ProxyArray = [4]string
+
+type ResponseWriteFunc func(w io.Writer) error
+
+type Container struct {
+	inters map[int]*Intermediary
+	count  int
+	uid    int
+	mu     sync.Mutex
+
+	processProxy    func(req *http.Request) ProxyArray
+	processRequest  func(req *http.Request) ResponseWriteFunc
+	processResponse func(resp *http.Response) ResponseWriteFunc
+}
+
+type MyRequest struct {
+	raw   *http.Request
+	proxy ProxyArray
+}
