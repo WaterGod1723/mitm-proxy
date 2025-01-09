7e4096767aeaa436a9ef1b9063e8b1c0896ef614
WaterGod1723 <87053221+WaterGod1723@users.noreply.github.com>
2025-01-09 17:06:58 +0800

    Update mitm.go

--- core/mitm.go
+++ core/mitm.go
@@ -324,3 +324,97 @@ func (inter *Intermediary) connectTarget(request *MyRequest, isConnClosed bool) 
 
 	return server, nil
 }
+
+func (inter *Intermediary) UpgradeClient2Tls(host string) error {
+	response := "HTTP/1.1 200 Connection Established\r\n\r\n"
+	if _, err := (*inter.client.conn).Write([]byte(response)); err != nil {
+		log.Printf("Error sending CONNECT response: %v", err)
+		return err
+	}
+
+	_host, _, err := net.SplitHostPort(host)
+	if err != nil {
+		_host = host
+	}
+	cert, err := SignHost([]string{_host})
+	if err != nil {
+		return err
+	}
+
+	inter.client.tlsConn = tls.Server(*inter.client.conn, &tls.Config{
+		Certificates: []tls.Certificate{
+			cert,
+		},
+		InsecureSkipVerify: true,
+	})
+
+	inter.client.isTls = true
+	inter.client.reader = bufio.NewReader(inter.client.tlsConn)
+	return nil
+}
+
+func (inter *Intermediary) UpgradeServer2TLS(req *MyRequest) error {
+	server := inter.server[req.raw.Host]
+	targetAddr := req.raw.Host
+	proxyAddr := req.proxy[1]
+
+	if !strings.Contains(targetAddr, ":") {
+		targetAddr += ":443"
+	}
+	// 如果需要通过代理连接
+	if proxyAddr != "" {
+		// 发送 CONNECT 请求到代理服务器
+		auth := strings.Join(req.proxy[2:], ":")
+		basicAuth := "Basic " + base64.StdEncoding.EncodeToString([]byte(auth))
+
+		// 构造 CONNECT 请求
+		connectReq := fmt.Sprintf("CONNECT %s HTTP/1.1\r\nHost: %s\r\nProxy-Authorization: %s\r\n\r\n", targetAddr, targetAddr, basicAuth)
+		if _, err := (*server.conn).Write([]byte(connectReq)); err != nil {
+			return fmt.Errorf("failed to send CONNECT request: %v", err)
+		}
+
+		resp, err := server.ReadResponse(nil)
+		if err != nil {
+			return fmt.Errorf("failed to read CONNECT response: %v", err)
+		}
+		defer resp.Body.Close()
+
+		// 检查代理服务器是否返回 200 Connection Established
+		if resp.StatusCode != http.StatusOK {
+			return fmt.Errorf("proxy returned non-200 status: %s", resp.Status)
+		}
+	}
+
+	// 将连接升级为 TLS 连接
+	server.tlsConn = tls.Client(*server.conn, &tls.Config{
+		ServerName:         strings.Split(targetAddr, ":")[0], // 设置 SNI
+		InsecureSkipVerify: true,
+	})
+	server.isTls = true
+
+	// 进行 TLS 握手
+	if err := server.tlsConn.Handshake(); err != nil {
+		return fmt.Errorf("TLS handshake failed: %v", err)
+	}
+	server.reader.Reset(*server.conn)
+	server.reader = bufio.NewReader(server.tlsConn)
+
+	return nil
+}
+
+func isWebSocketRequest(r *http.Request) bool {
+	// 检查请求头中的 Connection 和 Upgrade 字段
+	connection := r.Header.Get("Connection")
+	upgrade := r.Header.Get("Upgrade")
+	if !strings.Contains(strings.ToLower(connection), "upgrade") || !strings.EqualFold(upgrade, "websocket") {
+		return false
+	}
+
+	// 检查是否有 Sec-WebSocket-Key 和 Sec-WebSocket-Version 头
+	if r.Header.Get("Sec-WebSocket-Key") == "" || r.Header.Get("Sec-WebSocket-Version") == "" {
+		return false
+	}
+
+	// 如果符合所有条件，返回 true
+	return true
+}
